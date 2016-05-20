8dabb801447846dc3af353a720639de8e1a6585d
Ondřej Moravčík <moravcik.ondrej@gmail.com>
2016-05-20 12:44:59 +0200

    inaccessible files and current dir problem

--- lib/redmine-installer/steps/redmine_root.rb
+++ lib/redmine-installer/steps/redmine_root.rb
@@ -1,5 +1,9 @@
+require 'find'
+
 module Redmine::Installer::Step
   class RedmineRoot < Base
+
+    CHECK_N_INACCESSIBLE_FILES = 10
 
     def up
       # Get redmine root
@@ -8,12 +12,31 @@ module Redmine::Installer::Step
       # Make absolute path
       base.redmine_root = File.expand_path(base.redmine_root)
 
+      if base.redmine_root == Dir.pwd
+        error "You cannot upgrade current dir. Please go to the different directory."
+      end
+
       unless Dir.exist?(base.redmine_root)
         try_create_dir(base.redmine_root)
       end
 
       unless File.writable?(base.redmine_root)
         error t(:dir_is_not_writeable, dir: base.redmine_root)
+      end
+
+      inaccessible_files = []
+      Find.find(base.redmine_root).each do |item|
+        if !File.writable?(item) || !File.readable?(item)
+          inaccessible_files << item
+        end
+
+        if inaccessible_files.size > CHECK_N_INACCESSIBLE_FILES
+          break
+        end
+      end
+
+      if inaccessible_files.any?
+        error "Redmine root contains inaccessible files. Make sure that all files in #{base.redmine_root} are readable/writeable. (limit #{CHECK_N_INACCESSIBLE_FILES} files: #{inaccessible_files.join(', ')})"
       end
     end
 
